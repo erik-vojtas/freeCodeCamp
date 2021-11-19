c859701974fa3027de712494955be8c9caac0411
erik-vojtas <61457905+erik-vojtas@users.noreply.github.com>
2021-11-19 04:32:59 +0100

    Add files via upload

---
+++ Palindrome.js
@@ -0,0 +1,6 @@
+unction palindrome(str) {
+  str = str.toUpperCase().replace(/[^0-9a-z]/gi, '');
+  let reverseStr = str.split('').reverse().join(''); 
+  return reverseStr === str;
+}
+console.log(palindrome("A man, a plan, a canal. Panama"));
